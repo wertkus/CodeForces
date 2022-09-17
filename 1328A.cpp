92b536fe066e119d87e927557446df980bc18c02
Mert <100088180+wertkus@users.noreply.github.com>
2022-09-17 11:03:13 +0300

    Divisibility Problem

---
+++ 1328A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int n,a,b;
+	cin>>n;
+	while(n--){
+		cin>>a>>b;
+		if(a%b==0){cout<<0<<endl;}
+		else {cout<<b-a%b<<endl;}
+	}
+	
+	return 0;
+}
